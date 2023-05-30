db31c69c7538031a052f8761c986470f517e7201
U-Usaid <osaid.osaid66@gmail.com>
2023-05-30 13:27:39 +0400

    Q31

--- src/main/java/com/ScheduleCron/ScheduleCron/Schedule.java
+++ src/main/java/com/ScheduleCron/ScheduleCron/Schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 0 0 * 7 0-6")
+    @Scheduled(cron = "0 30 23 * * 5")
     public void schedule1(){
         System.out.println("Hello world");
     }
